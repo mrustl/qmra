bdf4536ee660d1dd8bb0eca0662b62f05546cbac
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-04-07 18:10:13 +0200

    Simplify by using new R package kwb.python

--- install_and_run_app.R
+++ install_and_run_app.R
@@ -3,10 +3,10 @@
 ### *** Note: Works Only on Windows! ***
 ################################################################################
 
-install.packages(c("fs", "reticulate"), repos = "https://cloud.r-project.org")
+install.packages("remotes", repos = "https://cloud.r-project.org")
+remotes::install_github("kwb-r/kwb.python")
 
-reticulate::install_miniconda()
-
+## Define dependencies (from "tools/requirements.txt")
 pkgs <- list(conda = c("django",
                        "django-crispy-forms",
                        "django-extensions",
@@ -21,75 +21,18 @@ pkgs <- list(conda = c("django",
              py = c("django-formtools")
              )
 
+### Define name of conda environment
 env_name <- "qmra"
 
-reticulate::conda_install(envname = env_name,
-                          packages = pkgs$conda)
-
-reticulate::use_condaenv(condaenv = env_name, required = TRUE)
-
-pyconfig <- reticulate::py_config()
-
-reticulate::use_python(python = pyconfig$python, required = TRUE)
-
-## on windows: (try this: https://github.com/rstudio/reticulate/issues/367#issuecomment-432920802)
-if(.Platform$OS.type == "windows") {
-  Sys.setenv(PATH = paste(PATH = pyconfig$pythonpath,
-                          Sys.getenv()["PATH"], sep=";")
-  )
-}
-
-reticulate::py_install(envname = env_name,
-                       packages = pkgs$py,
-                       pip = TRUE,
-                       pip_install_ignored = TRUE)
-
-conda_export <- function(condaenv, export_dir = getwd()) {
-  stopifnot(dir.exists(export_dir))
-  reticulate::use_miniconda(condaenv = condaenv, required = TRUE)
-  pyconfig <- reticulate::py_config()
-
-  ## on windows: (try this: https://github.com/rstudio/reticulate/issues/367#issuecomment-432920802)
-  if(.Platform$OS.type == "windows") {
-    Sys.setenv(PATH = paste(PATH = pyconfig$pythonpath,
-                            Sys.getenv()["PATH"], sep=";")
-    )
-  }
-
-cmds <- sprintf("conda activate %s && conda env export > %s",
-          condaenv,
-          file.path(export_dir, "environment.yml"))
-shell(cmd = cmds)
-}
+### Create conda environment and install conda/py packages in one call
+kwb.python::conda_py_install(env_name = env_name,
+                             pkgs = pkgs)
 
 ## Export python dependencies in yml
-conda_export("qmra", export_dir = "tools")
-
-run_django_qmra <- function(cmd = "runserver",
-                       condaenv = "qmra",
-                       path_manage.py = "tools/manage.py") {
-
-
-stopifnot(file.exists(path_manage.py))
-path_manage.py <- normalizePath(fs::path_abs(path_manage.py))
-reticulate::use_miniconda(condaenv = condaenv, required = TRUE)
-pyconfig <- reticulate::py_config()
-
-## on windows: (try this: https://github.com/rstudio/reticulate/issues/367#issuecomment-432920802)
-if(.Platform$OS.type == "windows") {
-  Sys.setenv(PATH = paste(PATH = pyconfig$pythonpath,
-                          Sys.getenv()["PATH"], sep=";")
-  )
-}
-
-cmds <- sprintf("conda activate %s && python %s %s",
-                  condaenv,
-                  path_manage.py,
-                  cmd)
-
-browseURL("http://127.0.0.1:8000", browser = NULL)
-shell(cmd = cmds, shell = "cmd.exe")
-}
+kwb.python::conda_export(condaenv = env_name, export_dir = "tools")
 
 ## Opens default browser on windows and start webserver
-run_django_qmra(condaenv = env_name)
+kwb.python::run_django(condaenv = env_name,
+                       cmd = "runserver",
+                       path_manage.py = "tools/manage.py")
+
